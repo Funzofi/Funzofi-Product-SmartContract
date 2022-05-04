220b69246941fe936cc0648341318aa0dd958255
Advaita Saha <adv11sphs@gmail.com>
2022-05-04 23:45:02 +0530

    custom data type added using struct to make data more accessible and contract less costly

--- contracts/Funzofi.sol
+++ contracts/Funzofi.sol
@@ -3,27 +3,39 @@ pragma solidity ^0.8.4;
 
 contract Funzofi {
 
+    // custom datatype using structs
+    struct player {
+        string  playerId;
+        int     score;
+        bool    present;
+    }
+
     // all the static variables holding the contract details
-    string public name;
-    string public gameDetails;
+    string  public name;
+    string  public gameDetails;
     address public owner;
 
     // all the variables holding the game data
     uint256 counter = 0;
+
     uint256 public prizePool;
     uint256 public entryFee;
     
-    mapping(address => uint) users;                         //  the users variable keep track of number of entries from a single account
-    mapping(string => int) public players;                  //  the players variable consits of the id od the players and their scores
-    mapping(uint => mapping(address => string)) entries;    //  the entries variable consits of the team data formed by the user for a particular entry
-    mapping(uint => int)[] public gameResult;               // the gameResult variable consits of the final rank list of the team along with their score in a sorted manner
+    mapping(address => uint)                                users;          //  the users variable keep track of number of entries from a single account
+    mapping(string  => player)                      public  players;        //  the players variable consits of the id od the players and their scores
+    mapping(uint    => mapping(address => string))          entries;        //  the entries variable consits of the team data formed by the user for a particular entry
+    mapping(uint    => int)[]                       public  gameResult;     //  the gameResult variable consits of the final rank list of the team along with their score in a sorted manner
 
-    constructor(string memory gameName, string memory description) {
-        name = gameName;
+    constructor(string memory gameName, string memory description, uint fee, string[] memory playersList) {
+        name        = gameName;
         gameDetails = description;
-        // players = [];
-        owner = msg.sender;
-    }
+        entryFee    = fee;
+        owner       = msg.sender;
 
+        // Loading the players data and initializing with 0 score
+        for(uint i=0; i<playersList.length; i++){
+            players[playersList[i]] = player(playersList[i], 0, true);
+        }
+    }
     
 }
--- test/Funzofi.test.js
+++ test/Funzofi.test.js
@@ -1,22 +1,31 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect, assert }  = require("chai");
+const { ethers }          = require("hardhat");
 
 let contract;
 let accounts;
 
 beforeEach( async () => {
-  accounts = await ethers.getSigners();
+  accounts      = await ethers.getSigners();
   const Funzofi = await ethers.getContractFactory("Funzofi");
-  contract = await Funzofi.deploy("CSK vs KKR", "test description");
+  contract      = await Funzofi.deploy("CSK vs KKR", "test description", 20, ['dhoni', 'mahi']);
   await contract.deployed();
 });
 
 describe("Funzofi Contract Tests", () => {
   it("Deployment Test and Data Check", async () => {
-      expect(await contract.name()).to.equal("CSK vs KKR");
-    });
+    const name      = await contract.name();
+    const details   = await contract.gameDetails();
+    const entryFee  = await contract.entryFee();
+    const player1   = await contract.players('dhoni');
+    const player2   = await contract.players('test');
+
+    assert(name == 'CSK vs KKR' && details == 'test description' && entryFee == 20);
+    expect(player1.present).to.equal(true);
+    expect(player1.score).to.equal(0);
+    expect(player2.present).to.equal(false);
+  });
 
   it("Owner Check", async () => {
-      expect(await contract.owner()).to.equal(accounts[0].address);
+    expect(await contract.owner()).to.equal(accounts[0].address);
   });
 });
