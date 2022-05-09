5d15fbc620ecbc215dd6e836bcdf428c4611da31
Advaita Saha <adv11sphs@gmail.com>
2022-05-09 12:35:09 +0530

    all functions implemented with basics tests for entry

--- contracts/Funzofi.sol
+++ contracts/Funzofi.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.13;
 
 contract Funzofi {
 
@@ -10,10 +10,17 @@ contract Funzofi {
         bool    present;
     }
 
+    struct entry {
+        address     userAddress;
+        string[]    team;
+        int         score;
+    }
+
     enum status {
         NOT_STARTED,
         ON_GOING,
-        ENDED
+        ENDED,
+        COMPLETED
     }
 
     // all the static variables holding the contract details
@@ -22,15 +29,14 @@ contract Funzofi {
     address public owner;
 
     // all the variables holding the game data
-    uint256        counter;
     status  public gameStatus;
-    uint256 public prizePool;
+    uint256 public prizePool    = 0;
     uint256 public entryFee;
     
-    mapping(address => uint)                        public  users;          //  the users variable keep track of number of entries from a single account
-    mapping(string  => player)                      public  players;        //  the players variable consits of the id od the players and their scores
-    mapping(uint    => mapping(address => string[]))        entries;        //  the entries variable consits of the team data formed by the user for a particular entry
-    mapping(uint    => int)[]                               gameResult;     //  the gameResult variable consits of the final rank list of the team along with their score in a sorted manner
+    mapping(string  => player)  public  players;        //  the players variable consits of the id od the players and their scores
+    mapping(address => uint)    public  users;          //  the users variable keep track of number of entries from a single account
+    entry[]                     public  entries;        //  the entries variable consits of the team data formed by the user for a particular entry
+    entry[]                     public  gameResult;     //  the gameResult variable consits of the final rank list of the team along with their score in a sorted manner
 
     // Modifier to give access only to the owner of the contract
     modifier onlyOwner(){
@@ -41,8 +47,32 @@ contract Funzofi {
         _;
     }
 
+    function sort(entry[] memory data) public pure returns(entry[] memory) {
+       quickSort(data, int(0), int(data.length - 1));
+       return data;
+    }
+
+    function quickSort(entry[] memory arr, int left, int right) internal pure {
+        int i = left;
+        int j = right;
+        if (i == j) return;
+        int pivot = arr[uint(left + (right - left) / 2)].score;
+        while (i <= j) {
+            while (arr[uint(i)].score > pivot) i++;
+            while (pivot > arr[uint(j)].score) j--;
+            if (i <= j) {
+                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
+    }
+
     constructor(string memory gameName, string memory description, uint fee, string[] memory playersList) {
-        counter     = 0;
         name        = gameName;
         gameDetails = description;
         entryFee    = fee;
@@ -58,47 +88,96 @@ contract Funzofi {
     function enterGame(string[] memory teamList) payable public {
         require(
             gameStatus == status.ON_GOING && users[msg.sender] < 5,
-            "Sorry! You have exceeded the number of entries"
+            "Sorry! You have exceeded the number of entries or Game hasn't started"
         );
         require(
-            msg.value == (entryFee * 1 ether),
+            msg.value == (entryFee * 1 wei),
             "Sorry! Incorrect entry fee"
         );
-        users[msg.sender]++;
+        bool check = true;
         for(uint i=0; i<teamList.length; i++){
-            if(players[teamList[i]].present){
-                entries[counter][msg.sender].push(teamList[i]);
+            if(players[teamList[i]].present == false){
+                check = false;
+                break;
+            }
+        }
+        require(check, "Sorry! Incorrect Team");
+        entries.push(entry(msg.sender, teamList, 0));
+        users[msg.sender]++;
+        prizePool += uint(msg.value);
+    }
+
+    function startGame() public onlyOwner {
+        require(
+            gameStatus == status.NOT_STARTED,
+            "Sorry! Either the game has already started or it has ended"
+        );
+        gameStatus = status.ON_GOING;
+    }
+
+    function cancelGame() public onlyOwner {
+        for(uint i=0; i<entries.length; i++){
+            payable(entries[i].userAddress).transfer(entryFee * 1 wei);
+        }
+    }
+
+    function endGame() public onlyOwner {
+        require(
+            gameStatus == status.ON_GOING,
+            "Sorry! Either the game has not already started or it has ended"
+        );
+        gameStatus = status.ENDED;
+
+        for(uint i=0; i<entries.length; i++){
+            string[] memory data = entries[i].team;
+            for(uint j=0; j<data.length; j++){
+                entries[i].score += players[data[j]].score;
+            }
+        }
+
+    }
+
+    function updateScore(player[] memory data) public onlyOwner {
+        require(
+            gameStatus == status.ON_GOING,
+            "Sorry! The hasn't started yet"
+        );
+        for(uint i=0; i<data.length; i++){
+            if(players[data[i].playerId].present){
+                players[data[i].playerId].score = data[i].score;
             } else {
                 revert();
             }
         }
-        counter++;
     }
 
-    function startGame() public onlyOwner {
-        gameStatus = status.ON_GOING;
+    function getWinnersList() public {
+        require(
+            gameStatus == status.ENDED,
+            "Sorry! The Game hasn't ended yet ended"
+        );
+        entry[] memory data = sort(entries);
+        for(uint i=0; i<data.length; i++){
+            gameResult.push(data[i]);
+        }
+        gameStatus = status.COMPLETED;
     }
 
-    function cancelGame() public onlyOwner {}
-
-    function endGame() public onlyOwner {
-        gameStatus = status.ENDED;
+    function declareWinner() public onlyOwner {
+        require(
+            gameStatus == status.COMPLETED,
+            "Sorry the winner list generation haven't been completed"
+        );
+        payable(gameResult[0].userAddress).transfer(prizePool * 1 wei);
     }
 
-    function updateScore() public onlyOwner {}
-
-    function getWinnersList() public view returns(uint[] memory) {
-        // require(
-        //     gameStatus == status.ENDED,
-        //     "Sorry! The Game hasn't ended yet ended"
-        // );
-        // uint[] memory list;
-        // for(uint i=0; i<gameResult.length; i++ ){
-        //     list.push(gameResult[i]);
-        // }
-        
+    function destroy() public onlyOwner {
+        require(msg.sender == owner, "You are not the owner");
+        selfdestruct(payable(owner));
     }
 
-    function declareWinner() public onlyOwner {}
+    receive() external payable {
+        revert();
+    }
     
 }
--- test/Funzofi.test.js
+++ test/Funzofi.test.js
@@ -7,7 +7,11 @@ let accounts;
 beforeEach( async () => {
   accounts      = await ethers.getSigners();
   const Funzofi = await ethers.getContractFactory("Funzofi");
-  contract      = await Funzofi.deploy("CSK vs KKR", "test description", 1, ['dhoni', 'mahi', 'chahal', 'sachin']);
+  contract      = await Funzofi.deploy(
+    "CSK vs KKR", "test description", 
+    ethers.utils.parseEther("1.0"), 
+    ['dhoni', 'mahi', 'chahal', 'sachin']
+  );
   await contract.deployed();
 });
 
@@ -15,9 +19,11 @@ describe("Funzofi Contract Tests", () => {
   it("Deployment Test and Data Check", async () => {
     const name      = await contract.name();
     const details   = await contract.gameDetails();
-    const entryFee  = await contract.entryFee();
+    const entryFee  = ethers.utils.formatEther(await contract.entryFee());
     const player1   = await contract.players('dhoni');
     const player2   = await contract.players('test');
+
+    console.log(entryFee);
 
     assert(name == 'CSK vs KKR' && details == 'test description' && entryFee == 1);
     expect(player1.present).to.equal(true);
@@ -46,6 +52,78 @@ describe("Funzofi Contract Tests", () => {
       {"value" : ethers.utils.parseEther("1.0")}
     );
     expect(await contract.connect(accounts[1]).users(accounts[1].address)).to.equal(1);
+  });
+
+  it("Update Score Check", async () => {
+    await contract.startGame();
+    await contract.updateScore([
+      ['dhoni', 20, true], 
+      ['mahi', 10, true]
+    ]);
+
+    const player1 = await contract.players('dhoni');
+    const player2 = await contract.players('chahal');
+    const player3 = await contract.players('mahi');
+
+    expect(player1.score).to.equal(20);
+    expect(player2.score).to.equal(0);
+    expect(player3.score).to.equal(10);
+  });
+
+  it("Endgame result check", async () => {
+    team = ['dhoni', 'mahi', 'sachin'];
+    await contract.startGame();
+    await contract.connect(accounts[1]).enterGame(
+      team,
+      {"value" : ethers.utils.parseEther("1.0")}
+    );
+
+    await contract.updateScore([
+      ['dhoni', 20, true], 
+      ['mahi', 10, true]
+    ]);
+
+    await contract.endGame();
+    expect(await contract.gameStatus()).to.equal(2);
+    const entryData = await contract.entries(0);
+    expect(entryData.score).to.equal(30);
+    
+  });
+
+  it("Winner declaration check", async () => {
+    team1 = ['dhoni', 'mahi', 'sachin'];
+    team2 = ['mahi', 'chahal'];
+    team3 = ['sachin', 'chahal'];
+    team4 = ['chahal'];
+    await contract.startGame();
+    await contract.connect(accounts[3]).enterGame(
+      team3,
+      {"value" : ethers.utils.parseEther("1.0")}
+    );
+    await contract.connect(accounts[2]).enterGame(
+      team2,
+      {"value" : ethers.utils.parseEther("1.0")}
+    );
+    await contract.connect(accounts[4]).enterGame(
+      team4,
+      {"value" : ethers.utils.parseEther("1.0")}
+    );
+    await contract.connect(accounts[1]).enterGame(
+      team1,
+      {"value" : ethers.utils.parseEther("1.0")}
+    );
+
+    await contract.updateScore([
+      ['dhoni', 20, true], 
+      ['mahi', 10, true],
+      ['chahal', 5, true],
+      ['sachin', 2, true]
+    ]);
+
+    await contract.endGame();
+    await contract.getWinnersList();
+    const data = await contract.gameResult(0);
+    expect(data.score).to.equal(32);
   });
 
 });
