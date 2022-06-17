0e7ea3e0745ca502f0c4c28365c04f03bbe619ed
Advaita Saha <adv11sphs@gmail.com>
2022-06-17 20:15:49 +0530

    factory contract tests completed

--- test/Factory.test.js
+++ test/Factory.test.js
@@ -5,14 +5,33 @@ let contract;
 let accounts;
 let FunzofiFactory;
 let FunzofiChildFactory;
+let FunzofiChildContract;
 
 // deployment of the factory contract 
 beforeEach( async () => {
     accounts                = await ethers.getSigners();
     FunzofiFactory          = await ethers.getContractFactory("FunzofiFactory");
     FunzofiChildFactory     = await ethers.getContractFactory("Funzofi");
-    contract                = FunzofiFactory.deploy();
+    contract                = await FunzofiFactory.deploy();
     await contract.deployed();
+
+    await contract.createGame(
+        "CSK vs KKR", "test description", 
+        ethers.utils.parseEther("1.0"), 
+        [
+        ['id01','dhoni', 0, true],
+        ['id02','mahi', 0, true],
+        ['id03','virat', 0, true],
+        ['id04','chahal', 0, true],
+        ['id05','sachin', 0, true],
+        ['id06','yuvraj', 0, true],
+        ['id07','shami', 0, true],
+        ['id08','kale', 0, true],
+        ]
+    );
+
+    const addr              = await contract.FunzofiGames(0);
+    FunzofiChildContract    = FunzofiChildFactory.attach(addr);
 });
 
 describe("Factory Contract Tests", () => {
@@ -41,11 +60,44 @@ describe("Factory Contract Tests", () => {
         } catch (err) {
             assert(false, err);
         }
-        const addr = await contract.FunzofiGames(0);
-        const FunzofiChildContract  = await FunzofiChildFactory.attach(addr);
+        
         const name = await FunzofiChildContract.name();
         expect(name).to.equal("CSK vs KKR");
         
+    });
+
+    it("StartGame function check in factory format", async () => {
+        try {
+            await contract.startGame(0);
+            assert(true);
+        } catch (err) {
+            assert(false, err);
+        }
+
+        const gameStat = await FunzofiChildContract.gameStatus();
+        expect(gameStat).to.equal(1);
+    });
+
+    it("Update score function check in factory format", async () => {
+        try {
+            await contract.startGame(0);
+            await contract.updateScore(0, [
+                ['id01','dhoni', 20, true],
+                ['id02','mahi', 10, true],
+                ['id03','virat', 0, true],
+            ]);
+            assert(true);
+        } catch (err) {
+            assert(false, err);
+        }
+
+        const player1 = await FunzofiChildContract.players('id01');
+        const player2 = await FunzofiChildContract.players('id03');
+        const player3 = await FunzofiChildContract.players('id02');
+
+        expect(player1.score).to.equal(20);
+        expect(player2.score).to.equal(0);
+        expect(player3.score).to.equal(10);
     });
 
 });
